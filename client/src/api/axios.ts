f93e69de461688d5fc55580cf3e7a5bebc3b221d
roiyraz88 <roiyraz88@gmail.com>
2025-04-09 11:26:13 +0300

    log addition

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+
+console.log("🔗 VITE_API_URL = ", import.meta.env.VITE_API_URL);
 const instance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true, 
