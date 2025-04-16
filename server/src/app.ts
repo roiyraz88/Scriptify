1b7c4d404e0c8dedb7e935a74ac23e151535011c
roiyraz88 <roiyraz88@gmail.com>
2025-04-16 16:31:47 +0300

    app.ts fix

--- server/src/app.ts
+++ server/src/app.ts
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://scriptify-two.vercel.app"], 
+    origin: ["http://localhost:5173", "https://scriptify-two.vercel.app/"], 
     credentials: true,
   })
 );
