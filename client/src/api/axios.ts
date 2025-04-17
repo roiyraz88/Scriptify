00358c56833ba61c3c1cb48b16dce2c7698f1a9b
roiyraz88 <roiyraz88@gmail.com>
2025-04-17 11:56:18 +0300

    fixes

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -2,31 +2,13 @@ import axios from "axios";
 
 const isLocalhost = window.location.hostname === "localhost";
 
+console.log("🔗 VITE_API_URL =", import.meta.env.VITE_API_URL);
+
 const instance = axios.create({
   baseURL: isLocalhost
     ? "http://localhost:5000"
     : import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
-
-instance.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    const message = error?.response?.data?.message;
-
-    if (
-      error.response?.status === 401 ||
-      message === "Invalid token" ||
-      message === "Token expired"
-    ) {
-      localStorage.removeItem("accessToken");
-      localStorage.removeItem("refreshToken");
-
-      window.location.href = "/";
-    }
-
-    return Promise.reject(error);
-  }
-);
 
 export default instance;
