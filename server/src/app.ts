4e0bdb0490cdf4b519a86fe842c611957301e7ab
roiyraz88 <roiyraz88@gmail.com>
2025-04-18 16:45:29 +0300

    app fix again

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,33 +1,49 @@
 import express from "express";
-import dotenv from "dotenv";
-import cors from "cors";
-import cookieParser from "cookie-parser";
-
-import authRoutes from "./routes/authRoutes";
-import scriptRoutes from "./routes/scriptRoutes";
-import profileRoutes from "./routes/profileRoutes";
-
-dotenv.config();
-
-const app = express();
-
-// const allowedOrigins = [
-//   "http://localhost:5173",
-//   "https://scriptify-two-blue.vercel.app",
-// ];
-
-app.use(cors({
-  origin: "https://scriptify-two-blue.vercel.app", 
-  credentials: true, 
-}));
-
-app.options("*", cors());
-
-app.use(express.json());
-app.use(cookieParser());
-
-app.use("/auth", authRoutes);
-app.use("/scripts", scriptRoutes);
-app.use("/profile", profileRoutes);
-
-export default app;
+ import dotenv from "dotenv";
+ import cors from "cors";
+ import cookieParser from "cookie-parser";
+ 
+ import authRoutes from "./routes/authRoutes";
+ import scriptRoutes from "./routes/scriptRoutes";
+ import profileRoutes from "./routes/profileRoutes";
+ 
+ dotenv.config();
+ 
+ const app = express();
+ 
+ const allowedOrigins = [
+   "http://localhost:5173",
+   "https://scriptify-two-blue.vercel.app",
+ ];
+ 
+ app.use(cors({
+   origin: allowedOrigins,
+   credentials: true,
+ }));
+ 
+ 
+ app.use(
+   cors({
+     origin: (origin, callback) => {
+       const allowed = [
+         "http://localhost:5173",
+         "https://scriptify-two-blue.vercel.app",
+       ];
+       if (!origin || allowed.includes(origin)) {
+         callback(null, true);
+       } else {
+         callback(new Error("Not allowed by CORS"));
+       }
+     },
+     credentials: true,
+   })
+ );
+ 
+ app.use(express.json());
+ app.use(cookieParser());
+ 
+ app.use("/auth", authRoutes);
+ app.use("/scripts", scriptRoutes);
+ app.use("/profile", profileRoutes);
+ 
+ export default app;
