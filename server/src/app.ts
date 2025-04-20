5c8c6a511a3f4768cb07a227248893d9ac3f4167
roiyraz88 <roiyraz88@gmail.com>
2025-04-20 10:56:38 +0300

    Changed the server.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -12,32 +12,19 @@ import express from "express";
  const app = express();
  
  const allowedOrigins = [
-   "http://localhost:5173",
-   "https://scriptify-two-blue.vercel.app",
- ];
- 
- app.use(cors({
-   origin: allowedOrigins,
-   credentials: true,
- }));
- 
- 
- app.use(
-   cors({
-     origin: (origin, callback) => {
-       const allowed = [
-         "http://localhost:5173",
-         "https://scriptify-two-blue.vercel.app",
-       ];
-       if (!origin || allowed.includes(origin)) {
-         callback(null, true);
-       } else {
-         callback(new Error("Not allowed by CORS"));
-       }
-     },
-     credentials: true,
-   })
- );
+  "https://scriptify-two-blue.vercel.app",
+];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+}));
  
  app.get("/health", (req, res) => {
   res.send("✅ Backend is alive!");
